46b55abe42bc165d06c5ed87d64648aa7fc60c17
Xiongfei Zhang <xzhang41@uncc.edu>
2020-10-04 16:30:08 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 30
+        },
+        {
+            title: 'Rent',
+            budget: 3500
+        },
+        {
+            title: 'Grocery',
+            budget: 90
+        },
+    ]
+};
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
